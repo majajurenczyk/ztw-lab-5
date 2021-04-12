c3223e74937fb019fe7177739ee824654aab7c47
majajurenczyk <majka.jurenczyk@gmail.com>
2021-04-12 18:07:52 +0200

    crud added

--- src/main/java/com/edu/pwr/ztw/books/api/BookController.java
+++ src/main/java/com/edu/pwr/ztw/books/api/BookController.java
@@ -4,12 +4,10 @@ import com.edu.pwr.ztw.books.model.Book;
 import com.edu.pwr.ztw.books.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.UUID;
 
 @RestController
 public class BookController {
@@ -30,6 +28,18 @@ public class BookController {
         return bookService.getBooks();
     }
 
+    @RequestMapping(path = "books/get/{id}", method = RequestMethod.GET)
+    public Book getBookById(@PathVariable("id") UUID id){
+        return bookService.getBookById(id).orElse(null);
+    }
 
+    @RequestMapping(path = "books/delete/{id}", method = RequestMethod.DELETE)
+    public void deleteBookById(@PathVariable("id") UUID id){
+        bookService.deleteBook(id);
+    }
 
+    @RequestMapping(path = "books/update/{id}", method = RequestMethod.PUT)
+    public void updateBookById(@PathVariable("id") UUID id, @NonNull @RequestBody Book bookToUpdate){
+        bookService.updateBook(id, bookToUpdate);
+    }
 }
